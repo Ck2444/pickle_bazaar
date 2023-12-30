aef037019b6e0170800a06816c6666f3f425d5fa
Konstantin <t777tt88@gmai.com>
2023-12-30 21:46:50 +0100

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <nav className="#43a047 green darken-1">
       <div className="nav-wrapper">
-        <a href="#!" className="brand-logo">
+        <a href="/" className="brand-logo">
         Pixel Bazaar
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="https://github.com/Ck2444/Pixel-Bazaar">Repo</a>
+            <a href="https://github.com/Ck2444/pickle_bazaar" target="_blank" rel="noreferrer">Repo</a>
           </li>
         </ul>
       </div>
